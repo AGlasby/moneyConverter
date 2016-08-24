372f312ab4194a3f720c22c17c4e325a0dabaee1
Alan Glasby <alan@glasby.me.uk>
2016-08-24 17:06:40 +0100

    Initial Commit

---
+++ moneyConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  moneyConverter
+//
+//  Created by Alan Glasby on 24/08/2016.
+//  Copyright © 2016 Alan Glasby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ moneyConverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  moneyConverter
+//
+//  Created by Alan Glasby on 24/08/2016.
+//  Copyright © 2016 Alan Glasby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
